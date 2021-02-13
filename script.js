3f4517de1697a3492f0fd529b415779aa24c4f49
codessbabe <oana.anghelachi@gmail.com>
2021-02-13 23:55:50 +0100

    Create HTML dynamically for transation item

--- script.js
+++ script.js
@@ -13,6 +13,23 @@ const dummyTransactions = [
   {id:2, text: "Salary", amount: 2500}, 
   {id:3, text: "Book", amount: -10}, 
   {id:4, text: "Clothes", amount: 200} 
-]
+];
 
 let transactions = dummyTransactions;
+
+// Add transactions to DOM list
+function addTransactionDOM(transaction) { 
+  // Get sign 
+  const sign = transaction.amount < 0 ? '-' : '+'; 
+  const item = document.createElement('li');
+
+
+  // Add class based on value
+  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+  item.innerHTML = ` 
+    ${transaction.text} 
+    <span>${sign}${Math.abs(transaction.amount)}</span>
+    <button class="delete-btn">x</button>
+  `;
+
+}
