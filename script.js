f4c0b5d717bae74d0a845013281686f5fb6d8233
codessbabe <oana.anghelachi@gmail.com>
2021-02-17 00:02:09 +0100

    Display balance, income and expense

--- script.js
+++ script.js
@@ -38,16 +38,16 @@ function addTransactionDOM(transaction) {
 // Update the balance, income and expense 
 function updateValues() { 
   const amounts = transactions.map(transaction => transaction.amount); 
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2); 
 
-
+  console.log(total);
 }
-
 
 
 // Init app 
 function init() { 
   list.innerHTML = ''; 
-  
+
   transactions.forEach(addTransactionDOM);
   updateValues()
 }
